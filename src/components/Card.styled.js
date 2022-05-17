e788e73decfc89377429b293b07bc374e655be63
Kakai <kenkai24@yahoo.com>
2022-05-18 01:53:45 +0200

    style overhaul (purple theme)

--- src/components/Card.styled.js
+++ src/components/Card.styled.js
@@ -1,5 +1,27 @@
 import styled from "styled-components";
 import gradient3 from "../images/gradient-3.png";
+import gradient4 from "../images/gradient-4.png";
+
+// const Card = styled.div`
+// position: absolute;
+// top: 3em;
+// display: flex;
+// flex-direction: column;
+// align-items: center;
+// justify-content: center;
+// gap: 2rem;
+// height: 25rem;
+// width: 20rem;
+// padding: 0 1.5rem;
+// outline: 1px solid transparent;
+// //background: #4C2E05;
+// background: url(${gradient3});
+// background-position: top;
+// border-radius: 16px;
+// border: 1px solid rgba(255, 255, 255, 0.2);
+// -webkit-transform-origin: 50%  51%;
+// // -webkit-backface-visibility: hidden;
+// `
 
 const Card = styled.div`
 position: absolute;
@@ -14,8 +36,9 @@ width: 20rem;
 padding: 0 1.5rem;
 outline: 1px solid transparent;
 //background: #4C2E05;
-background: url(${gradient3});
-background-position: top;
+background: url(${gradient4});
+//background-color: rgba(41, 45, 48, 1);
+background-position: center;
 border-radius: 16px;
 border: 1px solid rgba(255, 255, 255, 0.2);
 -webkit-transform-origin: 50%  51%;
